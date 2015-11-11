fad2ce14ebc5e9789a04263b7c3fe73f8c22d7af
luminousbeam <jocelyn.jeriah@gmail.com>
2015-11-11 14:53:27 -0500

    added view and template for detailed blog post as well as css for site

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -16,4 +17,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title 
+        return self.title
